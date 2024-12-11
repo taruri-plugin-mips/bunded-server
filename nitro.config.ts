7035b594f6da48489158b468e010b7511091d087
elonehoo <hi@elonehoo.me>
2024-12-11 18:46:42 +0800

    feat: update release api

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,8 @@
 //https://nitro.build/config
 export default defineNitroConfig({
   srcDir: "src",
-  compatibilityDate: "2024-12-11"
+  compatibilityDate: "2024-12-11",
+  runtimeConfig: {
+    folder: "/root/code", // `dev_token` is the default value
+  }
 });
---
+++ src/api/release.post.ts
@@ -0,0 +1,48 @@
+import {resolve} from 'pathe'
+import { createWriteStream } from 'node:fs'
+import { pipeline, PassThrough } from 'node:stream'
+
+export default defineEventHandler(async (event) => {
+  const body = await readMultipartFormData(event)
+  // not file upload
+  if (!body) {
+    throw createError({
+      status: 422,
+      statusMessage: "Bad Request",
+      message: "Not Found multipart file upload",
+      data: { file: "none" }
+    })
+  }
+  // save file to disk
+  const folder = useRuntimeConfig(event).folder
+
+  const allFiles = body.filter((item) => item.type)
+
+  const filePromise = allFiles.map((item) => {
+    const fileName = item.filename
+    const filePath = resolve(folder, fileName)
+    const wirteStream = createWriteStream(filePath)
+    let data = item.data as Buffer
+
+    const readStream = new PassThrough().end(data)
+
+    return new Promise((resolve, reject) => {
+      pipeline(readStream, wirteStream, (err) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        resolve({
+          url: `http://localhost:3000/uploads/${fileName}`,
+          type: item.type
+        })
+      })
+    })
+  })
+
+  let res = await Promise.all(filePromise)
+  return res
+})
+
+
