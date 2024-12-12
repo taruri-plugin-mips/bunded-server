257886c06c40e6ae0ffc209ec9923838b18c5975
elonehoo <hi@elonehoo.me>
2024-12-12 10:59:43 +0800

    fix: update release api

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,8 @@
-//https://nitro.build/config
+// https://nitro.build/config
 export default defineNitroConfig({
-  srcDir: "src",
-  compatibilityDate: "2024-12-11",
+  srcDir: 'src',
+  compatibilityDate: '2024-12-11',
   runtimeConfig: {
-    folder: "/root/code", // `dev_token` is the default value
-  }
-});
+    folder: '/root/code', // `dev_token` is the default value
+  },
+})
--- src/api/release.post.ts
+++ src/api/release.post.ts
@@ -1,6 +1,10 @@
-import {resolve} from 'pathe'
+import type { Buffer } from 'node:buffer'
 import { createWriteStream } from 'node:fs'
-import { pipeline, PassThrough } from 'node:stream'
+import { PassThrough, pipeline } from 'node:stream'
+import AdmZip from 'adm-zip'
+import { remove } from 'fs-extra'
+import path, { resolve } from 'pathe'
+import { useCreateFolder } from '~/utils/useCreateFolder'
 
 export default defineEventHandler(async (event) => {
   const body = await readMultipartFormData(event)
@@ -8,43 +12,69 @@ export default defineEventHandler(async (event) => {
   if (!body) {
     throw createError({
       status: 422,
-      statusMessage: "Bad Request",
-      message: "Not Found multipart file upload",
-      data: { file: "none" }
+      statusMessage: 'Bad Request',
+      message: 'Not Found multipart file upload',
+      data: { file: 'none' },
     })
   }
   // save file to disk
   const folder = useRuntimeConfig(event).folder
 
-  const allFiles = body.filter((item) => item.type)
+  const allFiles = body.filter(item => item.type)
 
-  const filePromise = allFiles.map((item) => {
+  const filePromise = allFiles.map(async (item) => {
     const fileName = item.filename
-    // todo: reg to version and create folder
-    const filePath = resolve(folder, fileName)
+    // filename is ${project-name}-${version}-release.zip
+    // eslint-disable-next-line regexp/no-super-linear-backtracking, regexp/no-misleading-capturing-group
+    const regex = /(.+)-(.+)-release\.zip/
+    // want get project-name and version
+    const match = fileName.match(regex)
+    if (!match) {
+      throw createError({
+        status: 422,
+        statusMessage: 'Bad Request',
+        message: 'Invalid file name',
+        data: { file: fileName },
+      })
+    }
+    const projectName = match[1]
+    const version = match[2]
+
+    // create folder if not exist
+    const folderPath = resolve(folder, projectName, `v${version}`)
+    await useCreateFolder(folderPath)
+    const filePath = resolve(folderPath, fileName)
     // write stream
     const writeStream = createWriteStream(filePath)
-    let data = item.data as Buffer
+
+    const data = item.data as Buffer
 
     const readStream = new PassThrough().end(data)
 
     return new Promise((resolve, reject) => {
-      pipeline(readStream, writeStream, (err) => {
+      pipeline(readStream, writeStream, async (err) => {
         if (err) {
           reject(err)
           return
         }
 
+        // create release folder
+        const releaseFolder = path.resolve(folderPath, 'release')
+        await useCreateFolder(releaseFolder)
+        // unzip
+        const zipfile = new AdmZip(filePath)
+        zipfile.extractAllTo(releaseFolder, true)
+        // remove zip file
+        await remove(filePath)
+
         resolve({
           url: `http://localhost:3000/uploads/${fileName}`,
-          type: item.type
+          type: item.type,
         })
       })
     })
   })
 
-  let res = await Promise.all(filePromise)
+  const res = await Promise.all(filePromise)
   return res
 })
-
-
